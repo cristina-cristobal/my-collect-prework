8168173ff0af08ea02af4578b6e2e03c788a7b67
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-29 15:45:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(array)
-i = 0
-first_names = []
-while i < array.length
-  first_names << yield array[i])
-  i += 1
-end
-first_names
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
