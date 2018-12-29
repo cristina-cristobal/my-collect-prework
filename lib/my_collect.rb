f16ba1387ac9915776c2f58e592c9ae6809cb433
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-29 15:47:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   first_names = []
   while i < array.length
-    yield first_names << (array[i])
+    first_names << yield(array[i])
     i += 1
   end
   first_names
